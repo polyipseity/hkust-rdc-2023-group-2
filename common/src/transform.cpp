672f8c0346979a6b63bb09c67700e4038c7bde0a
William So <polyipseity@gmail.com>
2023-11-05 23:22:28 +0800

    Limit max speed and use real values in code
    Signed-off-by: William So <polyipseity@gmail.com>

--- common/src/transform.cpp
+++ common/src/transform.cpp
@@ -2,8 +2,10 @@
 
 #include "util/matrix.hpp"
 
+#include <algorithm>
 #include <cmath>
 #include <tuple>
+#include <type_traits>
 #include <utility>
 
 PositionADRC::PositionADRC(decltype(m_position) position, decltype(m_velocity) velocity, decltype(m_gain) gain, double convergence) noexcept
@@ -21,9 +23,10 @@ auto PositionADRC::update(decltype(m_position) target, decltype(m_velocity) velo
 
 namespace
 {
-    constexpr auto const auto_robot_axle_radius{1.};                                      // todo: use real value
-    constexpr auto const auto_robot_rotate_velocity_diff_min{.01};                        // todo: find a good value
-    constexpr auto const auto_robot_self_rotate_radius_min{auto_robot_axle_radius / 10.}; // todo: find a good value
+    constexpr auto const auto_robot_axle_radius{.185};
+    constexpr auto const auto_robot_rotate_velocity_diff_min{.01};
+    constexpr auto const auto_robot_self_rotate_radius_min{auto_robot_axle_radius / 10.};
+    constexpr auto const auto_robot_max_velocity{18. / 5.};
     constexpr auto calc_linear_angular_velocities [[nodiscard]] (double left_v, double right_v) noexcept -> std::tuple<double, double, double>
     {
         // https://math.stackexchange.com/a/3680738, https://stackoverflow.com/a/55810955
@@ -44,10 +47,12 @@ namespace
     {
         if (std::abs(target_ang_v) < math::epsilon)
         {
+            target_lin_v = std::clamp(target_lin_v, -auto_robot_max_velocity, auto_robot_max_velocity);
             return {target_lin_v, target_lin_v};
         }
-        auto const target_rad_to_left{target_lin_v / target_ang_v};
-        return {target_ang_v * (target_rad_to_left - auto_robot_axle_radius), target_ang_v * (target_rad_to_left + auto_robot_axle_radius)};
+        auto const extra_v{target_ang_v * auto_robot_axle_radius};
+        target_lin_v = std::clamp(target_lin_v, -auto_robot_max_velocity + extra_v, auto_robot_max_velocity - extra_v);
+        return {target_lin_v - extra_v, target_lin_v + extra_v};
     }
 }
 
