ccb8ae2737e7256dde0bdc9299e138290523fcec
Ayn206 <108229140+Aien206@users.noreply.github.com>
2023-11-05 23:02:39 +0800

    Add files via upload
    Median_Blurring to massively reduce noise

--- auto_robot/color_recognition.py
+++ auto_robot/color_recognition.py
@@ -1,6 +1,3 @@
-# Python code for Multiple Color Detection 
-
-
 import numpy as np 
 import cv2 
 
@@ -16,7 +13,7 @@ webcam = cv2.VideoCapture(0)
 
 # Start a while loop 
 while(1): 
-	
+
 	# Reading the video from the 
 	# webcam in image frames 
 	_, imageFrame = webcam.read() 
@@ -30,7 +27,7 @@ while(1):
 	# Set range for red color and 
 	# define mask 
 	red_lower = np.array([136, 87, 111], np.uint8) 
-	red_upper = np.array([180, 255, 255], np.uint8) 
+	red_upper = np.array([200, 255, 255], np.uint8) 
 	red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
 
 	# Set range for green color and 
@@ -44,33 +41,41 @@ while(1):
 	blue_lower = np.array([94, 80, 2], np.uint8) 
 	blue_upper = np.array([120, 255, 255], np.uint8) 
 	blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
-	
+
 	# Morphological Transform, Dilation 
 	# for each color and bitwise_and operator 
 	# between imageFrame and mask determines 
 	# to detect only that particular color 
 	kernel = np.ones((5, 5), "uint8") 
-	
+
 	# For red color 
 	red_mask = cv2.dilate(red_mask, kernel) 
+	median_red = cv2.medianBlur(red_mask,15) 
 	res_red = cv2.bitwise_and(imageFrame, imageFrame, 
-							mask = red_mask) 
-	
-	# For green color 
-	green_mask = cv2.dilate(green_mask, kernel) 
-	res_green = cv2.bitwise_and(imageFrame, imageFrame, 
-								mask = green_mask) 
-	
-	# For blue color 
-	blue_mask = cv2.dilate(blue_mask, kernel) 
-	res_blue = cv2.bitwise_and(imageFrame, imageFrame, 
-							mask = blue_mask) 
+							mask = median_red) 
 
+	# For green color 
+	green_mask = cv2.dilate(green_mask, kernel)
+	median_green = cv2.medianBlur(green_mask,15) 
+	res_green = cv2.bitwise_and(imageFrame, imageFrame, 
+								mask = median_green) 
+
+	# For blue color 
+	blue_mask = cv2.dilate(blue_mask, kernel)
+	median_blue = cv2.medianBlur(blue_mask,15)
+	
+	#kernel = np.ones((15,15), np.float32)/255
+	#smoothed = cv2.filter2D(blue_mask, -1, kernel)
+	res_blue = cv2.bitwise_and(imageFrame, imageFrame, 
+							mask = median_blue) 
+    
 	# Creating contour to track red color 
 	contours, hierarchy = cv2.findContours(red_mask, 
 										cv2.RETR_TREE, 
-										cv2.CHAIN_APPROX_SIMPLE) 
+										cv2.CHAIN_APPROX_SIMPLE);
 	
+    
+
 	for pic, contour in enumerate(contours): 
 		area = cv2.contourArea(contour) 
 		if(area > working_area): 
@@ -78,7 +83,7 @@ while(1):
 			imageFrame = cv2.rectangle(imageFrame, (x, y), 
 									(x + w, y + h), 
 									(0, 0, 255), 2) 
-			
+
 			cv2.putText(imageFrame, "Red Colour", (x, y), 
 						cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
 						(0, 0, 255))	 
@@ -87,7 +92,7 @@ while(1):
 	contours, hierarchy = cv2.findContours(green_mask, 
 										cv2.RETR_TREE, 
 										cv2.CHAIN_APPROX_SIMPLE) 
-	
+
 	for pic, contour in enumerate(contours): 
 		area = cv2.contourArea(contour) 
 		if(area > working_area): 
@@ -96,13 +101,13 @@ while(1):
 			imageFrame = cv2.rectangle(imageFrame, (x, y), 
 									(x + w, y + h), 
 									(0, 255, 0), 2) 
-			
+
 			cv2.putText(imageFrame, "Green Colour", (x, y), 
 						cv2.FONT_HERSHEY_SIMPLEX, 
 						1.0, (0, 255, 0)) 
-			
 
-	
+
+
 
 	# Creating contour to track blue color 
 	contours, hierarchy = cv2.findContours(blue_mask, 
@@ -115,13 +120,18 @@ while(1):
 			imageFrame = cv2.rectangle(imageFrame, (x, y), 
 									(x + w, y + h), 
 									(255, 0, 0), 2) 
-			
+
 			cv2.putText(imageFrame, "Blue Colour", (x, y), 
 						cv2.FONT_HERSHEY_SIMPLEX, 
 						1.0, (255, 0, 0)) 
-			
+
 	# Program Termination 
+  
 	cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame) 
+	cv2.imshow("blue mask", res_blue)
+	cv2.imshow("red mask", res_red)
+	
+	
 	if cv2.waitKey(10) & 0xFF == ord('q'): 
 		cv2.destroyAllWindows() 
 		break
