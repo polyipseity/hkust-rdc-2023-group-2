2c6c253d32ce2e936679da425bc4d7db8a1f7aaa
john1q <manabayev.john1q@gmail.com>
2023-11-14 01:21:33 +0800

    feat: windows bluetooth

--- bluetooth.py
+++ bluetooth.py
@@ -1,8 +1,9 @@
 import serial
-ser = serial.Serial('/dev/tty.HC-05RDC2023Group22', 115200)  # open serial port
+#ser = serial.Serial('/dev/tty.HC-05RDC2023Group22', 115200)  # open serial port
+ser= serial.Serial('COM7')
 print(ser.name)         # check which port was really used
 
-ser.write(b'a')     # write a string
+ser.write(b'a\r\n')     # write a string
 ser.close()  
 
 
