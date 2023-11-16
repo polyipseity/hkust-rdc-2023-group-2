3196b1721e05253f408711c3eb9d86d18da9b6cd
john1q <manabayev.john1q@gmail.com>
2023-11-16 22:12:34 +0800

    Init

---
+++ app/src/main/java/com/sarmale/arduinobtexampleledcontrol/ConfigureLed.java
@@ -0,0 +1,277 @@
+package com.sarmale.arduinobtexampleledcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.InputDevice;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import java.lang.Thread;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.InputDevice;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+public class ConfigureLed extends AppCompatActivity {
+
+    private TextView textView;
+    private TextView textView2;
+    private TextView textView3;
+    private SeekBar seekBar;
+    Handler handler = new Handler();
+    Runnable runnable;
+    int delay = 25;
+
+    int stick_x, stick_y, btnx, btny, btnb, btna = 0;
+
+    int joystick_connected;
+
+    String message = String.valueOf(stick_x) + "." + String.valueOf(stick_y) + "." + String.valueOf(btna) + "." +
+            String.valueOf(btnb) + "." + String.valueOf(btnx) + "." + String.valueOf(btny);
+
+    ConnectedThread connectedThread;
+
+    private static final String TAG = "FrugalLogs";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configure_led);
+
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        seekBar = findViewById(R.id.seekBar);
+
+        connectedThread = MyApplication.getApplication().getCurrentConnectedThread();
+    }
+
+    @Override
+    protected void onResume() {
+        handler.postDelayed(runnable = new Runnable() {
+            public void run() {
+                handler.postDelayed(runnable, delay);
+                // send a text: 'a'
+                connectedThread.write(message + "\r\n");
+                btna = 0;
+                btnx = 0;
+                btnb = 0;
+                btny = 0;
+                message = String.valueOf(stick_x) + "." + String.valueOf(stick_y) + "." + String.valueOf(btna) + "." +
+                        String.valueOf(btnb) + "." + String.valueOf(btnx) + "." + String.valueOf(btny);
+
+            }
+        }, delay);
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable); //stop handler when activity not visible super.onPause();
+    }
+
+
+
+    @Override
+    public boolean dispatchGenericMotionEvent(MotionEvent event) {
+        if ((event.getSource() & InputDevice.SOURCE_JOYSTICK) == InputDevice.SOURCE_JOYSTICK &&
+                event.getAction() == MotionEvent.ACTION_MOVE) {
+            // Get X and Y axis values of the joystick
+            float xValue = getCenteredAxis(event, MotionEvent.AXIS_X);
+            float yValue = getCenteredAxis(event, MotionEvent.AXIS_Y);
+            stick_x = (int) (xValue * 100);
+            stick_y = (int) (yValue * 100);
+
+            // Use the X and Y values as needed (here, just displaying in TextView)
+            textView2.setText("Joystick X: " + stick_x + "\nJoystick Y: " + stick_y);
+
+            message = String.valueOf(stick_x) + "." + String.valueOf(stick_y) + "." + String.valueOf(btna) + "." +
+                    String.valueOf(btnb) + "." + String.valueOf(btnx) + "." + String.valueOf(btny);
+
+            textView3.setText(message);
+
+
+
+            return true; // To consume the event
+
+        }
+
+        return super.dispatchGenericMotionEvent(event);
+    }
+
+    private static float getCenteredAxis(MotionEvent event, int axis) {
+        InputDevice device = event.getDevice();
+        float flat = 0.5f;
+        float value = event.getAxisValue(axis);
+        final float deadZone = device.getMotionRange(axis).getFlat();
+        if (Math.abs(value) > deadZone) {
+            if (value > 0) {
+                return Math.min(value, 1);
+            } else {
+                return Math.max(value, -1);
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_BUTTON_X) {
+            // Change TextView value when "X" button is pressed
+            textView.setText("Button X Pressed!");
+            btnx = 1;
+
+            message = String.valueOf(stick_x) + "." + String.valueOf(stick_y) + "." + String.valueOf(btna) + "." +
+                    String.valueOf(btnb) + "." + String.valueOf(btnx) + "." + String.valueOf(btny);
+
+            textView3.setText(message);
+            // SHOUD HERE BE THE b \r\n??
+            return true; // To consume the event
+        }
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_BUTTON_Y) {
+            // Change TextView value when "X" button is pressed
+            textView.setText("Button Y Pressed!");
+            btny = 1;
+
+            message = String.valueOf(stick_x) + "." + String.valueOf(stick_y) + "." + String.valueOf(btna) + "." +
+                    String.valueOf(btnb) + "." + String.valueOf(btnx) + "." + String.valueOf(btny);
+
+            textView3.setText(message);
+            return true; // To consume the event
+        }
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_BUTTON_B) {
+            // Change TextView value when "X" button is pressed
+            textView.setText("Button B Pressed!");
+            btnb = 1;
+
+            message = String.valueOf(stick_x) + "." + String.valueOf(stick_y) + "." + String.valueOf(btna) + "." +
+                    String.valueOf(btnb) + "." + String.valueOf(btnx) + "." + String.valueOf(btny);
+
+            textView3.setText(message);
+            return true; // To consume the event
+        }
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_BUTTON_A) {
+            // Change TextView value when "X" button is pressed
+            textView.setText("Button A Pressed!");
+            btna = 1;
+
+            message = String.valueOf(stick_x) + "." + String.valueOf(stick_y) + "." + String.valueOf(btna) + "." +
+                    String.valueOf(btnb) + "." + String.valueOf(btnx) + "." + String.valueOf(btny);
+            textView3.setText(message);
+            return true; // To consume the event
+        }
+
+
+
+
+        //connectedThread = MyApplication.getApplication().getCurrentConnectedThread();
+
+        return super.dispatchKeyEvent(event);
+
+
+    }
+}
+
+
+
+//    private static final String TAG = "FrugalLogs";
+//    TextView tvProgressLabelRed,tvProgressLabelGreen,tvProgressLabelBlue;
+//    static final  char RED='R';
+//    static final  char GREEN='G';
+//    static final  char BLUE='B';
+//    int redNumber, greenNumber, blueNumber;
+//    ConnectedThread connectedThread;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_configure_led);
+//        SeekBar seekBarRed = findViewById(R.id.seekBarRed);
+//        SeekBar seekBarGreen = findViewById(R.id.seekBarGreen);
+//        SeekBar seekBarBlue = findViewById(R.id.seekBarBlue);
+//        ImageView colorDemo = findViewById(R.id.colorDemo);
+//        redNumber=greenNumber=blueNumber=0;
+//        colorDemo.setBackgroundColor(Color.rgb(redNumber, greenNumber, blueNumber));
+//        tvProgressLabelRed = findViewById(R.id.redValue);
+//        tvProgressLabelGreen = findViewById(R.id.greenValue);
+//        tvProgressLabelBlue = findViewById(R.id.blueValue);
+//        seekBarRed.setTag(RED);
+//        seekBarGreen.setTag(GREEN);
+//        seekBarBlue.setTag(BLUE);
+//        Log.d(TAG, "Launching ConfigureLed Activity");
+//
+//        // set a change listener on the SeekBar
+//        SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+//
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                // updated continuously as the user slides the thumb
+//                char color = (char) seekBar.getTag();
+//                switch(color) {
+//                    case RED:
+//                        tvProgressLabelRed.setText("Red: " + progress);
+//                        redNumber=progress;
+//                        break;
+//                    case GREEN:
+//                        tvProgressLabelGreen.setText("Green: " + progress);
+//                        greenNumber=progress;
+//                        break;
+//                    case BLUE:
+//                        tvProgressLabelBlue.setText("Blue: " + progress);
+//                        blueNumber=progress;
+//                        break;
+//                    default:
+//                        throw new IllegalStateException("Unexpected value: " + color);
+//                }
+//
+//                colorDemo.setBackgroundColor(Color.rgb(redNumber, greenNumber, blueNumber));
+//
+//            }
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//                // called when the user first touches the SeekBar
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//                // called after the user finishes moving the SeekBar
+//                String complete = String.valueOf(redNumber)  + "." + String.valueOf(greenNumber) +"."+String.valueOf(blueNumber);
+//
+//                connectedThread.write("a");
+//
+//            }
+//        };
+//
+//        seekBarRed.setOnSeekBarChangeListener(seekBarChangeListener);
+//        seekBarGreen.setOnSeekBarChangeListener(seekBarChangeListener);
+//        seekBarBlue.setOnSeekBarChangeListener(seekBarChangeListener);
+//
+//        int progress = seekBarRed.getProgress();
+//        tvProgressLabelRed.setText("Red: " + progress);
+//        tvProgressLabelGreen.setText("Green: " + progress);
+//        tvProgressLabelBlue.setText("Blue: " + progress);
+//
+//        connectedThread = MyApplication.getApplication().getCurrentConnectedThread();
+//        Log.d(TAG, "");
+//    }
+//}
